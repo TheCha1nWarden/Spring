2d76f27d24955f7c3cec283b76680fbbd0f73ca9
Artem <artem.krugman14@gmail.com>
2022-09-19 19:57:19 +0300

    add homework

---
+++ Homework7/src/main/java/com/my/homework/User.java
@@ -0,0 +1,38 @@
+package com.my.homework;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, length = 1024)
+    private String password;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Roles role;
+
+    public User(String username) {
+        this.username = username;
+    }
+}
